3d12982fe47370e3ae4286577aa305bb7b81f174
nasa9084 <nasa.9084.bassclarinet@gmail.com>
2019-11-07 17:19:00 +0900

    add test for matchRuntimeExpr

--- unmarshalyaml_test.go
+++ unmarshalyaml_test.go
@@ -42,3 +42,48 @@ func TestIsOneOf(t *testing.T) {
 		})
 	}
 }
+
+func TestMatchRuntimerExpr(t *testing.T) {
+	tests := []struct {
+		expr string
+		want bool
+	}{
+		{
+			expr: "$method",
+			want: true,
+		},
+		{
+			expr: "$request.header.accept",
+			want: true,
+		},
+		{
+			expr: "$request.path.id",
+			want: true,
+		},
+		{
+			expr: "$request.body#/user/uuid",
+			want: true,
+		},
+		{
+			expr: "$url",
+			want: true,
+		},
+		{
+			expr: "$response.body#/status",
+			want: true,
+		},
+		{
+			expr: "$response.header.Server",
+			want: true,
+		},
+	}
+	for i, tt := range tests {
+		t.Run(strconv.Itoa(i)+"/"+tt.expr, func(t *testing.T) {
+			got := matchRuntimeExpr(tt.expr)
+			if got != tt.want {
+				t.Errorf("unexpected: %t != %t", got, tt.want)
+				return
+			}
+		})
+	}
+}
