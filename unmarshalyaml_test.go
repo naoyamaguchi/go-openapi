2d372eea9a01d2a74a0fb77b57ce0ff50f98c683
nasa9084 <nasa.9084.bassclarinet@gmail.com>
2019-11-08 18:11:34 +0900

    add testcases

--- unmarshalyaml_test.go
+++ unmarshalyaml_test.go
@@ -1,7 +1,6 @@
 package openapi
 
 import (
-	"os"
 	"reflect"
 	"strconv"
 	"testing"
@@ -127,6 +126,56 @@ description: Development server`
 			return
 		}
 	})
+	t.Run("servers", func(t *testing.T) {
+		yml := `servers:
+- url: https://development.gigantic-server.com/v1
+  description: Development server
+- url: https://staging.gigantic-server.com/v1
+  description: Staging server
+- url: https://api.gigantic-server.com/v1
+  description: Production server`
+
+		var target struct {
+			Servers []*Server
+		}
+		if err := yaml.Unmarshal([]byte(yml), &target); err != nil {
+			t.Fatal(err)
+		}
+		servers := target.Servers
+		t.Run("0", func(t *testing.T) {
+			server := servers[0]
+			if server.url != "https://development.gigantic-server.com/v1" {
+				t.Errorf("unexpected server.url: %s", server.url)
+				return
+			}
+			if server.description != "Development server" {
+				t.Errorf("unexpected server.description: %s", server.description)
+				return
+			}
+		})
+		t.Run("1", func(t *testing.T) {
+			server := servers[1]
+			if server.url != "https://staging.gigantic-server.com/v1" {
+				t.Errorf("unexpected server.url: %s", server.url)
+				return
+			}
+			if server.description != "Staging server" {
+				t.Errorf("unexpected server.description: %s", server.description)
+				return
+			}
+		})
+		t.Run("2", func(t *testing.T) {
+			server := servers[2]
+			if server.url != "https://api.gigantic-server.com/v1" {
+				t.Errorf("unexpected server.url: %s", server.url)
+				return
+			}
+			if server.description != "Production server" {
+				t.Errorf("unexpected server.description: %s", server.description)
+				return
+			}
+		})
+	})
 	t.Run("with variables", func(t *testing.T) {
 		yml := `servers:
 - url: https://{username}.gigantic-server.com:{port}/{basePath}
@@ -146,7 +195,7 @@ description: Development server`
       default: v2`
 
 		var target struct {
-			Servers []*Server `yaml:"servers"`
+			Servers []*Server
 		}
 		if err := yaml.Unmarshal([]byte(yml), &target); err != nil {
 			t.Fatal(err)
@@ -186,6 +235,356 @@ description: Development server`
 			return
 		}
 	})
+}
+
+func TestComponentsUnmarshalYAML(t *testing.T) {
+	yml := `components:
+  schemas:
+    GeneralError:
+      type: object
+      properties:
+        code:
+          type: integer
+          format: int32
+        message:
+          type: string
+    Category:
+      type: object
+      properties:
+        id:
+          type: integer
+          format: int64
+        name:
+          type: string
+    Tag:
+      type: object
+      properties:
+        id:
+          type: integer
+          format: int64
+        name:
+          type: string
+  parameters:
+    skipParam:
+      name: skip
+      in: query
+      description: number of items to skip
+      required: true
+      schema:
+        type: integer
+        format: int32
+    limitParam:
+      name: limit
+      in: query
+      description: max records to return
+      required: true
+      schema:
+        type: integer
+        format: int32
+  responses:
+    NotFound:
+      description: Entity not found.
+    IllegalInput:
+      description: Illegal input for operation.
+    GeneralError:
+      description: General Error
+      content:
+        application/json:
+          schema:
+            $ref: '#/components/schemas/GeneralError'
+  securitySchemes:
+    api_key:
+      type: apiKey
+      name: api_key
+      in: header
+    petstore_auth:
+      type: oauth2
+      flows:
+        implicit:
+          authorizationUrl: http://example.org/api/oauth/dialog
+          scopes:
+            write:pets: modify pets in your account
+            read:pets: read your pets`
+	var target struct {
+		Components *Components
+	}
+	if err := yaml.Unmarshal([]byte(yml), &target); err != nil {
+		t.Fatal(err)
+	}
+	components := target.Components
+	schemas := components.schemas
+	t.Run("schemas.GeneralError", func(t *testing.T) {
+		generalError, ok := schemas["GeneralError"]
+		if !ok {
+			t.Error("schemas.GeneralError is not found")
+			return
+		}
+		if generalError.type_ != "object" {
+			t.Errorf("unexpected schema.type: %s", generalError.type_)
+			return
+		}
+		code, ok := generalError.properties["code"]
+		if !ok {
+			t.Error("schemas.GeneralError.properties.code is not found")
+			return
+		}
+		if code.type_ != "integer" {
+			t.Errorf("unexpected schemas.GeneralError.properties.code.type: %s", code.type_)
+			return
+		}
+		if code.format != "int32" {
+			t.Errorf("unexpected schemas.GeneralError.properties.code.format: %s", code.format)
+			return
+		}
+		message, ok := generalError.properties["message"]
+		if !ok {
+			t.Error("schemas.GeneralError.properties.message is not found")
+			return
+		}
+		if message.type_ != "string" {
+			t.Errorf("unexpected schemas.GeneralError.properties.message.type: %s", message.type_)
+			return
+		}
+	})
+	t.Run("schemas.Category", func(t *testing.T) {
+		category, ok := schemas["Category"]
+		if !ok {
+			t.Error("schemas.Category is not found")
+			return
+		}
+		if category.type_ != "object" {
+			t.Errorf("unexpected schema.Category.type: %s", category.type_)
+			return
+		}
+		id, ok := category.properties["id"]
+		if !ok {
+			t.Error("schemas.Category.properties.id is not found")
+			return
+		}
+		if id.type_ != "integer" {
+			t.Errorf("unexpected schemas.Category.properties.id.type: %s", id.type_)
+			return
+		}
+		if id.format != "int64" {
+			t.Errorf("unexpected schemas.Category.properties.id.format: %s", id.format)
+			return
+		}
+		name, ok := category.properties["name"]
+		if !ok {
+			t.Error("schemas.Category.properties.name is not found")
+			return
+		}
+		if name.type_ != "string" {
+			t.Errorf("unexpected schemas.Category.properties.name.type: %s", name.type_)
+			return
+		}
+	})
+	parameters := components.parameters
+	t.Run("parameters.skipParam", func(t *testing.T) {
+		skipParam, ok := parameters["skipParam"]
+		if !ok {
+			t.Error("parameters.skipParam is not found")
+			return
+		}
+		if skipParam.name != "skip" {
+			t.Errorf("unexpected parameters.skipParam.name: %s", skipParam.name)
+			return
+		}
+		if skipParam.in != "query" {
+			t.Errorf("unexpected parameters.skipParam.in: %s", skipParam.in)
+			return
+		}
+		if skipParam.description != "number of items to skip" {
+			t.Errorf("unexpected parameters.skipParam.description: %s", skipParam.description)
+			return
+		}
+		if skipParam.required != true {
+			t.Errorf("unexpected parameters.skipParam.required: %t", skipParam.required)
+			return
+		}
+		schema := skipParam.schema
+		if schema.type_ != "integer" {
+			t.Errorf("unexpected parameters.skipParam.schema.type: %s", schema.type_)
+			return
+		}
+		if schema.format != "int32" {
+			t.Errorf("unexpected parameters.skipParam.schema.format: %s", schema.format)
+			return
+		}
+	})
+	t.Run("parameters.limitParam", func(t *testing.T) {
+		limitParam, ok := parameters["limitParam"]
+		if !ok {
+			t.Error("parameters.limitParam is not found")
+			return
+		}
+		if limitParam.name != "limit" {
+			t.Errorf("unexpected parameters.limitParam.name: %s", limitParam.name)
+			return
+		}
+		if limitParam.in != "query" {
+			t.Errorf("unexpected parameters.limitParam.in: %s", limitParam.in)
+			return
+		}
+		if limitParam.description != "max records to return" {
+			t.Errorf("unexpected parameters.limitParam.description: %s", limitParam.description)
+			return
+		}
+		if limitParam.required != true {
+			t.Errorf("unexpected parameters.limitParam.required: %t", limitParam.required)
+			return
+		}
+		schema := limitParam.schema
+		if schema.type_ != "integer" {
+			t.Errorf("unexpected parameters.limitParam.schema.type: %s", schema.type_)
+			return
+		}
+		if schema.format != "int32" {
+			t.Errorf("unexpected parameters.limitParam.schema.format: %s", schema.format)
+			return
+		}
+	})
+	responses := components.responses
+	t.Run("responses.NotFound", func(t *testing.T) {
+		notFound, ok := responses["NotFound"]
+		if !ok {
+			t.Error("responses.NotFound is not found")
+			return
+		}
+		if notFound.description != "Entity not found." {
+			t.Errorf("unexpected responses.NotFound.description: %s", notFound.description)
+			return
+		}
+	})
+	t.Run("responses.IllegalInput", func(t *testing.T) {
+		illegalInput, ok := responses["IllegalInput"]
+		if !ok {
+			t.Error("responses.IllegalInput is not found")
+			return
+		}
+		if illegalInput.description != "Illegal input for operation." {
+			t.Errorf("unexpected responses.IllegalInput.description: %s", illegalInput.description)
+			return
+		}
+	})
+	t.Run("responses.GeneralError", func(t *testing.T) {
+		generalError, ok := responses["GeneralError"]
+		if !ok {
+			t.Error("responses.GeneralError is not found")
+			return
+		}
+		if generalError.description != "General Error" {
+			t.Errorf("unexpected responses.GeneralError.description: %s", generalError.description)
+			return
+		}
+		mediaType, ok := generalError.content["application/json"]
+		if !ok {
+			t.Error("responses.GeneralError.content.application/json is not found")
+			return
+		}
+		if mediaType.schema.reference != "#/components/schemas/GeneralError" {
+			t.Errorf("unexpected responses.GeneralError.content.application/json.schema.$ref")
+			return
+		}
+	})
+	securitySchemes := components.securitySchemes
+	t.Run("securitySchemes.api_key", func(t *testing.T) {
+		apiKey, ok := securitySchemes["api_key"]
+		if !ok {
+			t.Error("securitySchemes.api_key is not found")
+			return
+		}
+		if apiKey.type_ != "apiKey" {
+			t.Errorf("unexpected securitySchemes.api_key.type: %s", apiKey.type_)
+			return
+		}
+		if apiKey.name != "api_key" {
+			t.Errorf("unexpected securitySchemes.api_key.name: %s", apiKey.name)
+			return
+		}
+		if apiKey.in != "header" {
+			t.Errorf("unexpected securitySchemes.api_key.in: %s", apiKey.in)
+			return
+		}
+	})
+	t.Run("securitySchemes.petstore_auth", func(t *testing.T) {
+		petstoreAuth, ok := securitySchemes["petstore_auth"]
+		if !ok {
+			t.Error("securitySchemes.petstore_auth is not found")
+			return
+		}
+		if petstoreAuth.type_ != "oauth2" {
+			t.Errorf("unexpected securitySchemes.petstore_auth.type: %s", petstoreAuth.type_)
+			return
+		}
+		if petstoreAuth.flows.implicit.authorizationURL != "http://example.org/api/oauth/dialog" {
+			t.Errorf("unexpected securitySchemes.petstore_auth.flows.implicit.authorizationURL: %s", petstoreAuth.flows.implicit.authorizationURL)
+			return
+		}
+		scopes := petstoreAuth.flows.implicit.scopes
+		write, ok := scopes["write:pets"]
+		if !ok {
+			t.Error("securitySchemes.petstore_auth.flows.implicit.scopes.write:pets is not found")
+			return
+		}
+		if write != "modify pets in your account" {
+			t.Errorf("unexpected securitySchemes.petstore_auth.flows.implicit.scopes.write:pets: %s", write)
+		}
+		read, ok := scopes["read:pets"]
+		if !ok {
+			t.Error("securitySchemes.petstore_auth.flows.implicit.scopes.read:pets is not found")
+			return
+		}
+		if read != "read your pets" {
+			t.Errorf("unexpected securitySchemes.petstore_auth.flows.implicit.scopes.read:pets: %s", write)
+		}
+	})
+}
+
+func TestPathsUnmarshalYAML(t *testing.T) {
+	yml := `/pets:
+  get:
+    description: Returns all pets from the system that the user has access to
+    responses:
+      '200':
+        description: A list of pets.
+        content:
+          application/json:
+            schema:
+              type: array
+              items:
+                $ref: '#/components/schemas/pet'`
+	var paths Paths
+	if err := yaml.Unmarshal([]byte(yml), &paths); err != nil {
+		t.Fatal(err)
+	}
+	if _, ok := paths.paths["/pets"]; !ok {
+		t.Error("paths./pets is not found")
+		return
+	}
+	op := paths.paths["/pets"].get
+	if op.description != "Returns all pets from the system that the user has access to" {
+		t.Errorf("unexpected paths./pets.get.description: %s", op.description)
+		return
+	}
+	response, ok := op.responses.responses["200"]
+	if !ok {
+		t.Error("paths./pets.get.responses.200 is not found")
+		return
+	}
+	if _, ok := response.content["application/json"]; !ok {
+		t.Error("paths./pets.get.responses.200.content.application/json is not found")
+		return
+	}
+	schema := response.content["application/json"].schema
+	if schema.type_ != "array" {
+		t.Errorf("unexpected paths./pets.get.responses.200.content.application/json.schema.type: %s", schema.type_)
+		return
+	}
+	if schema.items.reference != "#/components/schemas/pet" {
+		t.Errorf("unexpected paths./pets.get.responses.200.content.application/json.schema.items.$ref: %s", schema.reference)
+		return
+	}
 }
 
 func TestIsOneOf(t *testing.T) {
@@ -269,12 +668,4 @@ func TestMatchRuntimerExpr(t *testing.T) {
 			}
 		})
 	}
-}
-
-func mustOpen(filename string) *os.File {
-	f, err := os.Open(filename)
-	if err != nil {
-		panic(err)
-	}
-	return f
 }
