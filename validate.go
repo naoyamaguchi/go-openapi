14fcbadcaa58a877b15f823d90254df0c9dc3b16
nasa9084 <nasa.9084.bassclarinet@gmail.com>
2019-11-19 11:52:08 +0900

    fix validation

--- validate.go
+++ validate.go
@@ -8,28 +8,29 @@ import (
 func validateURLTemplate(s string) error {
 	var scheme, address, port, path string
 	rest := s
-	splitScheme := strings.SplitN(rest, "://", 1)
+	splitScheme := strings.SplitN(rest, "://", 2)
 	if len(splitScheme) == 2 {
 		scheme = splitScheme[0]
 		rest = splitScheme[1]
 	} else {
 		rest = splitScheme[0]
 	}
-	splitPort := strings.SplitN(rest, ":", 1)
-	if len(splitPort) == 2 {
-		address = splitPort[0]
-		rest = splitPort[1]
-	} else {
-		rest = splitPort[0]
-	}
-	splitPath := strings.SplitN(rest, "/", 1)
+
+	splitPath := strings.SplitN(rest, "/", 2)
 	if len(splitPath) == 2 {
-		port = splitPath[0]
+		rest = splitPath[0]
 		path = splitPath[1]
 	} else {
-		path = splitPath[0]
+		rest = splitPath[0]
 	}
 
+	splitPort := strings.SplitN(rest, ":", 2)
+	if len(splitPort) == 2 {
+		address = splitPort[0]
+		port = splitPort[1]
+	} else {
+		address = splitPort[0]
+	}
 	scheme = urlTemplateVarRegexp.ReplaceAllLiteralString(scheme, "http")
 	address = urlTemplateVarRegexp.ReplaceAllLiteralString(address, "placeholder")
 	port = urlTemplateVarRegexp.ReplaceAllLiteralString(port, "80")
