1097496ff2546eb8091dffd48a699e2b997662f3
nasa9084 <nasa.9084.bassclarinet@gmail.com>
2019-12-10 16:55:53 +0900

    make private concrete error types

--- errors.go
+++ errors.go
@@ -5,43 +5,43 @@ import (
 	"strconv"
 )
 
-type RequiredError struct {
+type requiredError struct {
 	RequiredField string
 }
 
 func ErrRequired(requiredField string) error {
-	return RequiredError{
+	return requiredError{
 		RequiredField: requiredField,
 	}
 }
 
-func (e RequiredError) Error() string {
+func (e requiredError) Error() string {
 	return fmt.Sprintf("%s field is required", strconv.Quote(e.RequiredField))
 }
 
-func (e RequiredError) Is(target error) bool {
-	if target, ok := target.(RequiredError); ok {
+func (e requiredError) Is(target error) bool {
+	if target, ok := target.(requiredError); ok {
 		return e.RequiredField == target.RequiredField
 	}
 	return false
 }
 
-type UnknownKeyError struct {
+type unknownKeyError struct {
 	Key string
 }
 
 func ErrUnknownKey(key string) error {
-	return UnknownKeyError{
+	return unknownKeyError{
 		Key: key,
 	}
 }
 
-func (e UnknownKeyError) Error() string {
+func (e unknownKeyError) Error() string {
 	return fmt.Sprintf("unknown key: %s", e.Key)
 }
 
-func (e UnknownKeyError) Is(target error) bool {
-	if target, ok := target.(UnknownKeyError); ok {
+func (e unknownKeyError) Is(target error) bool {
+	if target, ok := target.(unknownKeyError); ok {
 		return e.Key == target.Key
 	}
 	return false
