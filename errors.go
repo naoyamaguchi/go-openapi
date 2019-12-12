dee8a198b497704159c7c3b8d44f424580eb06ec
nasa9084 <nasa.9084.bassclarinet@gmail.com>
2019-12-12 18:12:28 +0900

    remove Is

--- errors.go
+++ errors.go
@@ -19,13 +19,6 @@ func (e requiredError) Error() string {
 	return fmt.Sprintf("%s field is required", strconv.Quote(e.RequiredField))
 }
 
-func (e requiredError) Is(target error) bool {
-	if target, ok := target.(requiredError); ok {
-		return e.RequiredField == target.RequiredField
-	}
-	return false
-}
-
 type unknownKeyError struct {
 	Key string
 }
@@ -38,11 +31,4 @@ func ErrUnknownKey(key string) error {
 
 func (e unknownKeyError) Error() string {
 	return fmt.Sprintf("unknown key: %s", e.Key)
-}
-
-func (e unknownKeyError) Is(target error) bool {
-	if target, ok := target.(unknownKeyError); ok {
-		return e.Key == target.Key
-	}
-	return false
 }
